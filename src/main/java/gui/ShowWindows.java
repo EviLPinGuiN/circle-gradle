86f39db9346e24eab8a36e73402d0f4fd10b28b1
Nail <shaihrazievnail@gmail.com>
2018-06-04 01:13:06 +0300

    add tree

--- src/main/java/gui/ShowWindows.java
+++ src/main/java/gui/ShowWindows.java
@@ -1,23 +1,19 @@
 package gui;
 
-import api.ApiClient;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.ColoredTreeCellRenderer;
+import com.intellij.ui.ScrollPaneFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
-import io.reactivex.schedulers.Schedulers;
-import javafx.application.Platform;
-import javafx.embed.swing.JFXPanel;
-import javafx.scene.Scene;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
-import javafx.stage.Stage;
-import model.GithubUserHolder;
-import model.UserHolder;
+import com.intellij.ui.treeStructure.SimpleTree;
+import com.intellij.ui.treeStructure.Tree;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 import java.awt.*;
 
 
@@ -25,16 +21,9 @@ public class ShowWindows implements ToolWindowFactory {
 
     private ToolWindow myToolWindow;
     private JPanel myToolWindowContent;
-    private JTextField txtURL = new JTextField("Hello World!");
-    private JButton startButton;
-    private JPanel panel1;
-    private Stage stage;
-    private WebView browser;
-    private JFXPanel jfxPanel;
-    private JButton swingButton;
-    private WebEngine webEngine;
+    private ColoredTreeCellRenderer coloredTreeCellRenderer;
+    private Tree buildTree;
     final StaticComponents stat = StaticComponents.getInstance();
-    private JList list1;
 
 
     @Override
@@ -42,14 +31,24 @@ public class ShowWindows implements ToolWindowFactory {
         myToolWindow = toolWindow;
 
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
-        initComponents();
-        Content content = contentFactory.createContent(panel1, "", false);
+        buildTree = createTree();
+
+        myToolWindowContent.setLayout(new BorderLayout());
+        myToolWindowContent.add(ScrollPaneFactory.createScrollPane(buildTree), BorderLayout.CENTER);
+
+        Content content = contentFactory.createContent(myToolWindowContent, "", false);
         toolWindow.getContentManager().addContent(content);
     }
 
-    private void initComponents() {
+    private Tree createTree() {
         String[] data = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5"};
-        list1.setListData(data);
+        String item = "Item";
+        SimpleTree tree = new SimpleTree();
+        tree.getEmptyText().setText("LOADING");
+        tree.setCellRenderer(coloredTreeCellRenderer);
+        tree.setName("buildTree");
+        tree.setModel(new DefaultTreeModel(new DefaultMutableTreeNode(item)));
+        return tree;
     }
 
     /**
@@ -59,25 +58,6 @@ public class ShowWindows implements ToolWindowFactory {
      * NOT on the AWT-EventQueue Thread
      */
     private void createScene() {
-        String token = stat.getToken();
-        ApiClient.getPhraseService()
-                .getProfile()
-                .map(UserHolder::getGithubUserHolder)
-                .map(GithubUserHolder::getGithubUser)
-                .subscribeOn(Schedulers.io())
-                .subscribe(githubUser -> {
-                    String login = githubUser.getLogin();
-                    String name = githubUser.getName();
-                });
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                WebView view = new WebView();
-                webEngine = view.getEngine();
-                webEngine.load("https://circleci.com/");
-                Scene scene = new Scene(view);
-                jfxPanel.setScene(scene);
-            }
-        });
+
     }
 }
